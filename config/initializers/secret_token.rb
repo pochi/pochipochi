26e7dc6c1e648beab623380d0a03829fc000344f
pochi <pochi.black@gmail.com>
2011-02-20 20:49:17 +0900

    Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pochipochi::Application.config.secret_token = '87e03a2996eda3b21d2ec82789c6cb7da2d8a033cc11c67a1ed9ad1291e42fc2e60b30ab618d8a1f85d90625da71bd5289d0aa0afb4a075842f7cedde3700e3a'
