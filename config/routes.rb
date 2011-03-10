112e566f88b34bf1480300797a058ed3bbd77c29
pochi <pochi.black@gmail.com>
2011-03-10 11:21:33 +0900

    Can log in and log out

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Pochipochi::Application.routes.draw do
+  get 'log_in' => 'sessions#new', :as => 'log_in'
+  get 'log_out' => 'sessions#destroy', :as => 'log_out'
   get 'sign_up' => 'users#new', :as => 'sign_up'
   resources :users
+  resources :sessions
   resources :bookmarks
 
   root :to => "bookmarks#index"
