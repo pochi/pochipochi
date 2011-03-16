bcab2e673cb4a107a77bfc1a691bda9b8409e483
pochi <pochi.black@gmail.com>
2011-03-16 21:29:39 +0900

    Modified migrate file

--- db/migrate/20110316073706_add_name_to_provider.rb
+++ db/migrate/20110316073706_add_name_to_provider.rb
@@ -1,11 +1,11 @@
 class AddNameToProvider < ActiveRecord::Migration
   def self.up
-    add_column :providers, :name, :string
+    # add_column :providers, :name, :string
     remove_column :providers, :user_id
   end
 
   def self.down
     add_column :providers, :user_id, :integer
-    remove_column :providers, :name
+    # remove_column :providers, :name
   end
 end
