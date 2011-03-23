d2f61619fc2bb467de497e51daee728dfced36cd
pochi <pochi.black@gmail.com>
2011-03-23 15:37:46 +0900

    Bookmark can usual link

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,7 +1,7 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 $(function(){
-  $("#bookmarks a").live('click', function(){
+  $("#bookmarks th a").live('click', function(){
     $.getScript(this.href);
     return false;
   });
